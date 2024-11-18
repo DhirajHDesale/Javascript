914342b1c5659b5b3ef34302dc4c9c2bfe5c1b4b
DhirajHDesale <dhirajhanumantpatil@gmail.com>
2024-11-18 08:20:16 +0530

    Variables in Javascript

---
+++ Variables.js
@@ -0,0 +1,35 @@
+let age = 24;
+var name = "Kiran";
+const gender = "Male";
+console.log(age);
+age = 25;
+console.log(age);
+
+console.log(name);
+name = "Shubham";
+console.log(name);
+
+console.log(gender);
+gender = "Female";
+console.log(gender);
+
+
+/*
+In Javascript We can declare variables by using var,let keyword
+var is blocked level and let is scope level
+it is good to use let keywords variable instead of var
+*/
+
+/*
+anything that you don't want to change then const keyword comes into the action.
+meaning that if you declare as const keyword the value can not be updated or modified.
+*/
+
+/*
+variables are containers to storing the data.
+*/
+
+
+/*
+if we declare the variables but not initialized the variable the value is printed as undefined.
+*/
